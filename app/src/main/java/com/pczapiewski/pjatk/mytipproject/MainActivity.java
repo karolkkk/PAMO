b3c204e517ea0c73073c597834868118365e8b0c
karolkkk <s15195@pjwstk.edu.pl>
2019-10-22 22:14:50 +0200

    finished

--- app/src/main/java/com/pczapiewski/pjatk/mytipproject/MainActivity.java
+++ app/src/main/java/com/pczapiewski/pjatk/mytipproject/MainActivity.java
@@ -13,20 +13,16 @@ import java.text.NumberFormat;
 
 public class MainActivity extends Activity {
 
-    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
-    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
 
     private double height = 0.0;
     private double weight = 0.0;
-    private double percent = 0.15;
-
     private TextView heightTextView;
     private TextView weightTextView;
-    private TextView percentTextView;
-    private TextView tipTextView;
+
+    private TextView bmiTextView;
     private TextView totalTextView;
 
-    // called when the activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // call superclass onCreate
@@ -36,7 +32,7 @@ public class MainActivity extends Activity {
         heightTextView = (TextView) findViewById(R.id.heightTextView);
         weightTextView= (TextView) findViewById(R.id.weightTextView);
        // percentTextView = (TextView) findViewById(R.id.percentTextView);
-        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        bmiTextView = (TextView) findViewById(R.id.bmiTextView);
         totalTextView = (TextView) findViewById(R.id.totalTextView);
 
         // set amountEditText's TextWatcher
@@ -48,27 +44,24 @@ public class MainActivity extends Activity {
 
     // calculate and display tip and total amounts
     private void calculate() {
-        // calculate the tip and total
-        //double tip = height * percent;
+        double m =height/100;
 
-        double tip = weight / (height * height);
+        double bmi = weight / (m * m);
+        bmiTextView.setText(String.valueOf(bmi));
 
-        // display tip and total formatted as currency
-        tipTextView.setText(String.valueOf(tip));
-
-        if (tip < 15) {
+        if (bmi < 15) {
             totalTextView.setText("very severely underweight");
-        } else if (tip >=15 && tip <= 16) {
+        } else if (bmi >=15 && bmi <= 16) {
             totalTextView.setText("severely underweight");
-        } else if (tip >16 && tip <= 18.5) {
+        } else if (bmi >16 && bmi <= 18.5) {
             totalTextView.setText("underweight");
-        } else if (tip >18.5 && tip <= 25) {
+        } else if (bmi >18.5 && bmi <= 25) {
             totalTextView.setText("healthy weight");
-        } else if (tip >25 && tip <= 30) {
+        } else if (bmi >25 && bmi <= 30) {
             totalTextView.setText("overweight");
-        } else if (tip >30 && tip <= 35) {
+        } else if (bmi >30 && bmi <= 35) {
             totalTextView.setText("moderately obese");
-        } else if (tip >35 && tip <= 40) {
+        } else if (bmi >35 && bmi <= 40) {
             totalTextView.setText("severely obese");
         } else {
             totalTextView.setText("very severely obese");
@@ -99,7 +92,7 @@ public class MainActivity extends Activity {
         public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
     };
     private final TextWatcher weightEditTextWatcher = new TextWatcher() {
-        // called when the user modifies the bill amount
+
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
 
